d2e44f4f25cffd9111722573c89b5a0346b66660
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2026-02-22 00:31:52 -0500

    config, migrations, public

---
+++ migrations/Version20260221033107.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260221033107 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE users (id INT IDENTITY NOT NULL, email NVARCHAR(180) NOT NULL, roles VARCHAR(MAX) NOT NULL, password NVARCHAR(255) NOT NULL, nombre NVARCHAR(255) NOT NULL, is_active BIT NOT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON users (email) WHERE email IS NOT NULL');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT IDENTITY NOT NULL, body VARCHAR(MAX) NOT NULL, headers VARCHAR(MAX) NOT NULL, queue_name NVARCHAR(190) NOT NULL, created_at DATETIME2(6) NOT NULL, available_at DATETIME2(6) NOT NULL, delivered_at DATETIME2(6), PRIMARY KEY (id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 ON messenger_messages (queue_name, available_at, delivered_at, id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA db_accessadmin');
+        $this->addSql('CREATE SCHEMA db_backupoperator');
+        $this->addSql('CREATE SCHEMA db_datareader');
+        $this->addSql('CREATE SCHEMA db_datawriter');
+        $this->addSql('CREATE SCHEMA db_ddladmin');
+        $this->addSql('CREATE SCHEMA db_denydatareader');
+        $this->addSql('CREATE SCHEMA db_denydatawriter');
+        $this->addSql('CREATE SCHEMA db_owner');
+        $this->addSql('CREATE SCHEMA db_securityadmin');
+        $this->addSql('CREATE SCHEMA dbo');
+        $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
