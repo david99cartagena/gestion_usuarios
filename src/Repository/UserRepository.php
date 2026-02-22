5b882addc293b996cb27a5c54be4e405541b92f5
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2026-02-22 18:31:40 -0500

    ajuste de control de login error

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
@@ -31,6 +32,17 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $user->setPassword($newHashedPassword);
         $this->getEntityManager()->persist($user);
         $this->getEntityManager()->flush();
+    }
+    
+    public function checkPreAuth(User $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->isActive()) {
+            throw new CustomUserMessageAuthenticationException('Tu cuenta está desactivada. Contacta al administrador.');
+        }
     }
 
     //    /**
